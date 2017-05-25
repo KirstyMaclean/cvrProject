d947c90897331edf4d4d613f596cf5396aae35d8
kmac94 <kirstymaclean49@gmail.com>
2017-05-25 13:43:03 +0100

    2nd Attempt at Parsing XML data from SRA

--- Scripts/sra_eSearch.pl
+++ Scripts/sra_eSearch.pl
@@ -17,20 +17,27 @@ print "Count = ",$factory->get_count,"\n";
 
 # UIDs
 my @ids = $factory->get_ids;
+#my @items = $factory-> get_all_Items;
 
 my $factory = Bio::DB::EUtilities->new(-eutil => 'esummary',
                                        -email => '2023085m@student.gla.ac.uk',
                                        -db    => 'sra',
                                        -id    => \@ids);
+#print "ID = ",$factory->get_ids,"\n";
+#print "Data = ",$factory->get_all_Items,"\n";
 
+								
+while( my $ds = $factory->next_DocSum){
+	print "ID: ",$ds->get_id,"\n";
 
-while (my $ds = $factory->next_DocSum) {
-    print "ID: ",$ds->get_id,"\n";
+#	print "Item: ",$ds->get_all_Items,"\n";
+
 #parsing revelant data from XML
 while (my $item = $ds->next_Item) {
-            print "Name: ",$item->get_name,"\n";
-            print "Data: ",$item->get_content,"\n";
-            print "Type: ",$item->get_type,"\n";
+	   print "ID: ", $item->get_id,"\n";	 
+           print "Name: ",$item->get_name,"\n";
+           print "Data: ",$item->get_content,"\n";
+           print "Type: ",$item->get_type,"\n";
 #        while (my $listitem = $ds->next_ListItem) {
             # do stuff here...
 #            while (my $listitem = $ds->next_Structure) {
