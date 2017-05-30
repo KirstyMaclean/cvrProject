51fd9cb0f6fd4074ea591a9e2e4049fb82e21f18
kmac94 <kirstymaclean49@gmail.com>
2017-05-30 16:36:26 +0100

    final script for downloading all sra enteries

--- Scripts/sra_eSearch_final.pl
+++ Scripts/sra_eSearch_final.pl
@@ -1,4 +1,3 @@
-
 use Bio::DB::EUtilities;
 use Bio::Tools::EUtilities;
 use Bio::Tools::EUtilities::Summary::Item;
@@ -13,7 +12,6 @@ my $factory = Bio::DB::EUtilities->new(-eutil => 'esearch',
 print "Query translation: ",$factory->get_query_translation,"\n";
 
 # query hits
-
 print "Count = ",$factory->get_count,"\n";
 my $count = $factory->get_count;
 
@@ -30,18 +28,29 @@ my $retry = 0; my ($retmax, $retstart) = (500,0);
 while ($retstart < $count) {
     $factory->set_parameters(-retmax => $retmax,
                              -retstart => $retstart);
+    eval{
+        $factory->get_Response(-cb =>
+            sub {my ($data) = @_; print $out $data} );
+    };
+    if ($@) {
+        die "Server error: $@.  Try again later" if $retry == 5;
+        print STDERR "Server error, redo #$retry\n";
+        $retry++;
+    }
+	print "Retrieved $retstart";
+    $retstart += $retmax;
+
+
+close $out;
 
 while (my $ds = $factory->next_DocSum){
   my ($id,$name,$design,$platform,$model,$bases,$date,$des,$study);
   $id=$ds->get_id;
   while (my $item = $ds->next_Item) {
-#     print "Name: ",$item->get_name,"\n";
      $name=$item->get_name;
      my $data=$item->get_content;
-#     print "Type: ",$item->get_type,"\n";
-#     print "$data\n";
-#<Summary><Title>Illumina HiSeq 2500 sequencing</Title><Platform instrument_model="Illumina HiSeq 2500">ILLUMINA</Platform><Statistics total_runs="1" total_spots="2458700" total_bases="159815500" total_size="112179603" load_done="true" cluster_name="public"/></Summary><Submitter acc="ERA886772" center_name="" contact_name="European Nucleotide Archive" lab_name="European Nucleotide Archive"/><Experiment acc="ERX2001348" ver="1" status="public" name="Illumina HiSeq 2500 sequencing"/><Study acc="ERP021057" name="Transposon_directed_insertion_site_sequencing_of_ISS1_libraries_of_Streptococcus_equi_recovered_from_ponies_with_strangles"/><Organism taxid="1336" CommonName="Streptococcus equi strain 4067"/><Sample acc="ERS1505374" name="Streptococcus equi strain 4067"/><Instrument ILLUMINA="Illumina HiSeq 2500"/><Library_descriptor><LIBRARY_NAME>NT928753E</LIBRARY_NAME><LIBRARY_STRATEGY>AMPLICON</LIBRARY_STRATEGY><LIBRARY_SOURCE>GENOMIC</LIBRARY_SOURCE><LIBRARY_SELECTION>PCR</LIBRARY_SELECTION><LIBRARY_LAYOUT> <SINGLE/> </LIBRARY_LAYOUT><LIBRARY_CONSTRUCTION_PROTOCOL>TraDIS pre quality controlled</LIBRARY_CONSTRUCTION_PROTOCOL></Library_descriptor><Bioproject>PRJEB19069</Bioproject><Biosample>SAMEA50444668</Biosample>
-   if($data=~/\<Summary><Title\>(.+)\<\/Title\>\<Platform instrument_model\=\"(.+)\"\>(.+)\<\/Platform\>\<Statistics total_runs\=\"(.+)\" total_spots\=\"(.+)\" total_bases\=\"(.+)\" total_size/){     
+
+if($data=~/\<Summary><Title\>(.+)\<\/Title\>\<Platform instrument_model\=\"(.+)\"\>(.+)\<\/Platform\>\<Statistics total_runs\=\"(.+)\" total_spots\=\"(.+)\" total_bases\=\"(.+)\" total_size/){     
   $design=$1;
       $model=$2;
        $platform=$3;
@@ -57,8 +66,7 @@ if($data=~/\<Study acc\=\"(.+)\" name\=\"(.+)\"\/\>\<Organism taxid/){
 		
 }	
    }
-   print "\nId: $id\nDesign: $design\nPlatform: $platform\nModel: $model\nNumber of Bases: $bases\nDate: $date\nDescription: $des\nStudy Accession: $study\n";
-}
+print "\nId: $id\nDesign: $design\nPlatform: $platform\nModel: $model\nNumber of Bases: $bases\nDate: $date\nDescription: $des\nStudy Accession: $study\n";
 
 }
-
+}
