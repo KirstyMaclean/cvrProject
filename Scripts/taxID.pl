30209bc548fadac70a1b566a90d914f0ba02c00c
Kirsty Maclean <Kirstymaclean49@gmail.com>
2017-06-20 10:19:47 +0100

    complete script

--- Scripts/taxID.pl
+++ Scripts/taxID.pl
@@ -1,18 +1,18 @@
-use Bio::DB::EUtilities;
+se Bio::DB::EUtilities;
 #creating array
 my @tArray;
 
 #opening text file of Taxonomic IDs
 open(my $fh, "<", "TaxID.txt")
     or die "Failed to open file: $!\n";
-while(<$fh>) { 
-    chomp; 
+while(<$fh>) {
+    chomp;
     push @tArray, $_;
-} 
+}
 close $fh;
 
 #printing the header for text file
-print "NCBI Scientific Name\tCommon Name\tLineage\tTaxonomic ID\n";
+print "Taxonomic ID\tNCBI Scientific Name\tCommon Name\tDivision\tGenus\tSpecies\tSubspecies\n";
 
 #for each taxonomic ID search NCBI taxonomic database for Scientific Name.
 foreach my $tArray(@tArray)
@@ -23,14 +23,22 @@ my $factory = Bio::DB::EUtilities->new(-eutil => 'esummary',
                                        -email => '2023085m@student.gla.ac.uk',
                                        -db    => 'taxonomy',
                                        -id    => $id );
-#BROKEN ATM parse out Scientific Name, Common Name and Lineage#
+#parse out Scientific Name, Common Name, Division, Genus, Species, Rank and Subspecies#
+
 my $summary=$factory->next_DocSum;
 
 my ($SciName) = $summary->get_contents_by_name('ScientificName');
-my ($ComName) = $summary->get_contents_by_name('CommonName'); #ERROR MESSAGE: Can't call method "get_contents_by_name" on an undefined value 
-my ($Lineage) = $summary->get_contents_by_name('Lineage');
-$Lineage=~s/\t//g; #ensure tabs are ignored in Lineage data 
+my ($ComName) = $summary->get_contents_by_name('CommonName');
+my ($diV) = $summary->get_contents_by_name('Division');
+my ($Genus)= $summary->get_contents_by_name('Genus');
+my ($Species)= $summary->get_contents_by_name('Species');
+#my ($Rank) = $summary->get_contents_by_name('Rank'); #not needed
+my ($Subsp) = $summary->get_contents_by_name('Subsp');
 
-#printing 
-print "$SciName\t$ComName\t$Lineage\t$id\n";
+
+
+#printing
+
+print "$id\t$SciName\t$ComName\t$diV\t$Genus\t$Species\t$Subsp\n";
 }
+
