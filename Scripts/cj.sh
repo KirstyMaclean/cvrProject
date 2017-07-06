cf41f08e92a25ad3ab7a6ea277a48cde79516734
Kirsty Maclean <Kirstymaclean49@gmail.com>
2017-07-06 15:27:45 +0100

    Update cj.sh

--- Scripts/cj.sh
+++ Scripts/cj.sh
@@ -4,7 +4,7 @@ perl  sra_shiny_redownload.pl > newestdata.txt |
 r curationScript1.R |
 perl taxID.pl > taxonData.txt |
 r curationScript2.R |
-r maps.R |
+r maps.R > ggmap.txt |
 r curationScript3.R
 
 
