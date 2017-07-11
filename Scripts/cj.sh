1ff18df8f3af22dae9b8623395bbb4f3925c3168
kmac94 <kirstymaclean49@gmail.com>
2017-07-11 11:26:55 +0100

    removing files to avoid error message

--- Scripts/cj.sh
+++ Scripts/cj.sh
@@ -4,6 +4,7 @@
 Rscript curationScript1.R 
 perl taxID.pl > taxonData.txt 
 Rscript curationScript2.R 
+rm Center_geocoded.rds
 Rscript maps.R  
 Rscript curationScript3.R
 cp ShinyData.txt /srv/shiny-server/SRAexplorer/.
