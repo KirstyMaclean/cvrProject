5eadcbe859320d4a3ba11bc6c0016b8a8a0f4c7a
kmac94 <kirstymaclean49@gmail.com>
2017-07-11 11:48:52 +0100

    adding time stamp

--- Scripts/cj.sh
+++ Scripts/cj.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-#perl  sra_shiny_redownload.pl > newestdata.txt | 
+current_time=$(date "+Y.%m.%d")
+echo "Current Time : $current_time"
+
+perl  sra_shiny_redownload.pl > newestdata.txt 
 Rscript curationScript1.R 
 perl taxID.pl > taxonData.txt 
 Rscript curationScript2.R 
@@ -8,6 +11,8 @@ rm Center_geocoded.rds
 Rscript maps.R  
 Rscript curationScript3.R
 cp ShinyData.txt /srv/shiny-server/SRAexplorer/.
+newfile="ShinyData".$current_time.".txt"
+mv ShinyData.txt $newfile
 
 
 
